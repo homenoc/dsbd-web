e740480dcbad543238052d05156ecd64af4d3837
yoneyan <github@yoneyan.net>
2020-06-30 01:54:52 +0900

    add url and module

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -4,6 +4,9 @@ import {NoticeComponent} from "./notice/notice.component";
 import {BaseComponent} from "./base/base.component";
 import {DashboardComponent} from "./dashboard.component";
 import {AgreementComponent} from "./registration/agreement/agreement.component";
+import {QuestionComponent} from "./registration/question/question.component";
+import {Contract1Component} from "./registration/contract1/contract1.component";
+import {Contract2Component} from "./registration/contract2/contract2.component";
 
 
 const routes: Routes = [{
@@ -13,7 +16,11 @@ const routes: Routes = [{
     {path: '', redirectTo: 'notice', pathMatch: 'full'},
     {path: 'notice', component: NoticeComponent},
     {path: 'base', component: BaseComponent},
-    {path: 'registration/agreement', component: AgreementComponent}
+    {path: 'registration/agreement', component: AgreementComponent},
+    {path: 'registration/question', component: QuestionComponent},
+    {path: 'registration/contract1', component: Contract1Component},
+    {path: 'registration/contract2', component: Contract2Component},
+
   ]
 }
 ];
