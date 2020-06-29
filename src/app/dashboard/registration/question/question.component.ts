dd39146f8ee76d756bca46a0f2e6ef1bebb42bce
yoneyan <github@yoneyan.net>
2020-06-30 02:22:26 +0900

    added handling of blanks

--- src/app/dashboard/registration/question/question.component.ts
+++ src/app/dashboard/registration/question/question.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {DataService} from "../../../service/data.service";
+import {CommonService} from "../../../service/common.service";
 
 @Component({
   selector: 'app-question',
@@ -11,6 +12,7 @@ export class QuestionComponent implements OnInit {
 
   constructor(
     private dataService: DataService,
+    private commonService: CommonService,
   ) {
   }
 
@@ -26,11 +28,15 @@ export class QuestionComponent implements OnInit {
 
 
   requestComment() {
-    this.dataService.addUserComment({
-      data1: this.base1.value,
-      data2: this.base2.value,
-      data3: this.base3.value,
-      data4: this.base4.value
-    })
+    if (this.base1.value == '' || this.base2.value == '' || this.base3.value == '' || this.base4.value == '') {
+      this.commonService.openBar('The field is blank.', 2000)
+    } else {
+      this.dataService.addUserComment({
+        data1: this.base1.value,
+        data2: this.base2.value,
+        data3: this.base3.value,
+        data4: this.base4.value
+      })
+    }
   }
 }
