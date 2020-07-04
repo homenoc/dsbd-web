0a1f579ba6476d9a13cfc48cedf92e98e6f52473
Yuto Yoneda <yoneda@local.bgp.ne.jp>
2020-07-05 03:17:13 +0900

    add not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegistrationComponent} from "./registration/registration.component";
 import {AuthGuard} from "./guard/auth.guard";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
 
 const routes: Routes = [
@@ -10,6 +11,8 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   // {path: 'registration', component: RegistrationComponent},
   {path: '', loadChildren: './dashboard/dashboard.module#DashboardModule', canActivate: [AuthGuard]},
+  {path: '**', component: NotFoundComponent},
+
 ];
 
 @NgModule({
