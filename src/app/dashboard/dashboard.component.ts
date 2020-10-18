699c596822bb7342caeaf405aaa609d3a68c1c0f
yoneyan <github@yoneyan.net>
2020-10-19 00:24:25 +0900

    add dashboard.component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,8 @@ import {MatSidenav} from '@angular/material/sidenav';
 import {UserService} from '../service/user.service';
 import {CommonService} from '../service/common.service';
 import {AuthService} from '../service/auth.service';
+import {Router} from '@angular/router';
+import {GroupService} from '../service/group.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,23 +17,42 @@ export class DashboardComponent implements OnInit, AfterViewInit {
 
   constructor(
     public userService: UserService,
+    private groupService: GroupService,
     private sidenavService: SidenavService,
     private commonService: CommonService,
     public authService: AuthService,
+    private router: Router,
   ) {
   }
 
-  public release = false;
-  public registration = false;
+  public notice = false;
+  public info = false;
+  public request = false;
   public gid = 0;
 
 
   ngOnInit() {
-    this.userService.getLoginUser().then(d => {
-      if (d.status) {
-        this.gid = d.data.gid;
+    this.userService.getLoginUser().then(user => {
+      if (user.status) {
+        if (user.data.status === 0) {
+          this.router.navigate(['/dashboard/setting']).then();
+        } else if (user.data.status >= 100) {
+          this.authService.logOut();
+        } else {
+          this.notice = true;
+          if (user.data.gid === 0) {
+            this.request = true;
+          }
+          this.groupService.get().then((group) => {
+            if (group.status) {
+              if (group.data.status < 1000) {
+                this.info = true;
+              }
+            }
+          }).catch();
+        }
       } else {
-        this.commonService.openBar(d.error, 4000);
+        this.commonService.openBar(user.error, 4000);
       }
     });
   }
