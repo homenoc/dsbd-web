828a0832f03a126db371d80a83122d96382f00b0
yoneyan <github@yoneyan.net>
2020-10-19 17:30:57 +0900

    add network service

---
+++ src/app/service/network.service.ts
@@ -0,0 +1,94 @@
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {CommonService} from './common.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NetworkService {
+
+  constructor(
+    public router: Router,
+    private commonService: CommonService,
+    private http: HttpClient
+  ) {
+  }
+
+  add(body): Promise<any> {
+    return this.http.post(environment.base.url + environment.base.path + '/group/network',
+      body, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          USER_TOKEN: sessionStorage.getItem('ClientID'),
+          ACCESS_TOKEN: sessionStorage.getItem('AccessToken'),
+        })
+      }).toPromise().then(r => {
+      const response: any = r;
+      console.log('response: ' + JSON.stringify(response));
+      if (response.status === true) {
+        return response;
+      } else {
+        return {
+          status: false,
+          error: response.error.error,
+          data: response
+        };
+      }
+    }).catch(error => {
+      console.log(error);
+      return {status: false, error};
+    });
+  }
+
+  get(): Promise<any> {
+    return this.http.get(environment.base.url + environment.base.path + '/group/network', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        USER_TOKEN: sessionStorage.getItem('ClientID'),
+        ACCESS_TOKEN: sessionStorage.getItem('AccessToken'),
+      }),
+    }).toPromise().then(r => {
+      const response: any = r;
+      if (response.status === true) {
+        return response;
+      } else {
+        return {
+          status: false,
+          error: response.error.error,
+          data: response
+        };
+      }
+    }).catch(error => {
+      console.log(error);
+      return {status: false, error};
+    });
+  }
+
+  confirm(body): Promise<any> {
+    return this.http.post(environment.base.url + environment.base.path + '/group/network/confirm',
+      body, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          USER_TOKEN: sessionStorage.getItem('ClientID'),
+          ACCESS_TOKEN: sessionStorage.getItem('AccessToken'),
+        })
+      }).toPromise().then(r => {
+      const response: any = r;
+      console.log('response: ' + JSON.stringify(response));
+      if (response.status === true) {
+        return response;
+      } else {
+        return {
+          status: false,
+          error: response.error.error,
+          data: response
+        };
+      }
+    }).catch(error => {
+      console.log(error);
+      return {status: false, error};
+    });
+  }
+}
