52a502b4f99c9d67c8107b61f7710c10e81643eb
yoneyan <github@yoneyan.net>
2020-10-19 19:21:57 +0900

    登録ページのstatus処理の変更

--- src/app/dashboard/registration/registration.component.ts
+++ src/app/dashboard/registration/registration.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {UserService} from '../../service/user.service';
 import {GroupService} from '../../service/group.service';
-import {AuthService} from "../../service/auth.service";
+import {AuthService} from '../../service/auth.service';
 
 @Component({
   selector: 'app-registration',
@@ -19,7 +19,8 @@ export class RegistrationComponent implements OnInit {
   ) {
   }
 
-  public loading = true;
+  // type| 0:なし 1:ネットワーク情報記入 2:接続情報記入
+  public type = 0;
 
   ngOnInit(): void {
     this.userService.getLoginUser().then((user) => {
@@ -29,8 +30,18 @@ export class RegistrationComponent implements OnInit {
           } else {
             this.groupService.get().then((group) => {
               if (group.status) {
-                if (1000 <= group.group.status) {
-                  this.authService.logOut();
+                const status = group.group.status;
+                if (status === 2 || status === 11 || status === 21 || status === 111 || status === 121) {
+                  this.router.navigate(['/dashboard/registration/network']).then();
+                }
+                if (status === 12 || status === 22 || status === 112 || status === 122) {
+                  this.type = 1;
+                }
+                if (status === 13 || status === 23 || status === 113 || status === 123) {
+                  this.router.navigate(['/dashboard/registration/connection']).then();
+                }
+                if (status === 14 || status === 24 || status === 114 || status === 124) {
+                  this.type = 2;
                 }
               }
             });
