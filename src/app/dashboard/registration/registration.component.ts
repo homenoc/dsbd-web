cc45e717abeddca10713227e144423d6e4068af8
yoneyan <github@yoneyan.net>
2020-10-30 22:32:31 +0900

    白背景になるバグの修正

--- src/app/dashboard/registration/registration.component.ts
+++ src/app/dashboard/registration/registration.component.ts
@@ -21,30 +21,35 @@ export class RegistrationComponent implements OnInit {
 
   // type| 0:なし 1:ネットワーク情報記入 2:接続情報記入
   public type = 0;
+  public lock = false;
 
   ngOnInit(): void {
     this.userService.getLoginUser().then((user) => {
         if (user.status) {
-          if (user.data.gid === 0) {
-            this.router.navigate(['/dashboard/registration/init']).then();
+          if (user.data.status === 0) {
+            this.lock = true;
           } else {
-            this.groupService.get().then((group) => {
-              if (group.status) {
-                const status = group.group.status;
-                if (status === 2 || status === 11 || status === 21 || status === 111 || status === 121) {
-                  this.router.navigate(['/dashboard/registration/network']).then();
+            if (user.data.group_id === 0) {
+              this.router.navigate(['/dashboard/registration/init']).then();
+            } else {
+              this.groupService.get().then((group) => {
+                if (group.status) {
+                  const status = group.group.status;
+                  if (status === 2 || status === 11 || status === 21 || status === 111 || status === 121) {
+                    this.router.navigate(['/dashboard/registration/network']).then();
+                  }
+                  if (status === 12 || status === 22 || status === 112 || status === 122) {
+                    this.type = 1;
+                  }
+                  if (status === 13 || status === 23 || status === 113 || status === 123) {
+                    this.router.navigate(['/dashboard/registration/connection']).then();
+                  }
+                  if (status === 14 || status === 24 || status === 114 || status === 124) {
+                    this.type = 2;
+                  }
                 }
-                if (status === 12 || status === 22 || status === 112 || status === 122) {
-                  this.type = 1;
-                }
-                if (status === 13 || status === 23 || status === 113 || status === 123) {
-                  this.router.navigate(['/dashboard/registration/connection']).then();
-                }
-                if (status === 14 || status === 24 || status === 114 || status === 124) {
-                  this.type = 2;
-                }
-              }
-            });
+              });
+            }
           }
         }
       }
