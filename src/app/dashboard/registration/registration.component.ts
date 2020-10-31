24850b1646f5c869506024bfa119f765d68e4c04
yoneyan <github@yoneyan.net>
2020-10-31 23:39:00 +0900

    登録ページの自動遷移機能の修正と追加

--- src/app/dashboard/registration/registration.component.ts
+++ src/app/dashboard/registration/registration.component.ts
@@ -19,7 +19,7 @@ export class RegistrationComponent implements OnInit {
   ) {
   }
 
-  // type| 0:なし 1:ネットワーク情報記入 2:接続情報記入
+  // type| 0:なし 1:初期審査 2:ネットワーク情報記入 3:接続情報記入
   public type = 0;
   public lock = false;
 
@@ -35,17 +35,20 @@ export class RegistrationComponent implements OnInit {
               this.groupService.get().then((group) => {
                 if (group.status) {
                   const status = group.group.status;
+                  if (status === 1) {
+                    this.type = 1;
+                  }
                   if (status === 2 || status === 11 || status === 21 || status === 111 || status === 121) {
                     this.router.navigate(['/dashboard/registration/network']).then();
                   }
                   if (status === 12 || status === 22 || status === 112 || status === 122) {
-                    this.type = 1;
+                    this.type = 2;
                   }
                   if (status === 13 || status === 23 || status === 113 || status === 123) {
                     this.router.navigate(['/dashboard/registration/connection']).then();
                   }
                   if (status === 14 || status === 24 || status === 114 || status === 124) {
-                    this.type = 2;
+                    this.type = 3;
                   }
                 }
               });
