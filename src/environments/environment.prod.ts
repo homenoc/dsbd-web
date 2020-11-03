226fd2715a26233300d4af01526b493e575afec1
yoneyan <github@yoneyan.net>
2020-11-03 16:06:14 +0900

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   api: {
-
+    url: 'http://dashboard.homenoc.ad.jp:8080',
+    path: '/api/v1',
+  },
+  websocket: {
+    url: 'ws://dashboard.homenoc.ad.jp:8080',
+    path: '/ws/v1'
   }
 };
