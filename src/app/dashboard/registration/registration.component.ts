b063478ba6eb46f094b1d99bff45be6858ba56c8
yoneyan <github@yoneyan.net>
2021-03-17 08:41:02 +0900

    [fix] ループバグの修正

--- src/app/dashboard/registration/registration.component.ts
+++ src/app/dashboard/registration/registration.component.ts
@@ -22,8 +22,8 @@ export class RegistrationComponent implements OnInit {
   // type| 0:なし 1:初期審査 2:ネットワーク情報記入 3:接続情報記入
   public type = 0;
   public status = 0;
-  public lock = false;
   public pass = false;
+  public groupExamination = false;
 
   ngOnInit(): void {
     this.userService.getLoginUser().then((user) => {
@@ -35,13 +35,17 @@ export class RegistrationComponent implements OnInit {
             this.status = group.status;
             this.pass = group.pass;
 
-            // ユーザ記入段階
-            if (this.status === 1) {
-              this.router.navigate(['/dashboard/registration/service']).then();
-            }
-            // ユーザ側接続記入段階
-            if (this.status === 3) {
-              this.router.navigate(['/dashboard/registration/connection']).then();
+            if (this.pass) {
+              // ユーザ記入段階
+              if (this.status === 1) {
+                this.router.navigate(['/dashboard/registration/service']).then();
+              }
+              // ユーザ側接続記入段階
+              if (this.status === 3) {
+                this.router.navigate(['/dashboard/registration/connection']).then();
+              }
+            } else {
+              this.groupExamination = true;
             }
           });
         }
