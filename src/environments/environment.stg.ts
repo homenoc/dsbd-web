183e77fc3c3d78ae957c3c7aee01e400139dc5ff
yoneyan <github@yoneyan.net>
2021-03-17 08:21:59 +0900

    [fix] URL変更

--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   api: {
-    url: 'https://dweb-dev-osk.homenoc.ad.jp',
+    url: 'https://dsys-dev-osk.homenoc.ad.jp',
     path: '/api/v1',
   },
   websocket: {
-    url: 'wss://dweb-dev-osk.homenoc.ad.jp',
+    url: 'wss://dsys-dev-osk.homenoc.ad.jp',
     path: '/ws/v1'
   }
 };
