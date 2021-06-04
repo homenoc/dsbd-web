6a0e22a60f811c4b851ef81cb2db54961219fe63
yoneyan <github@yoneyan.net>
2021-06-05 04:20:08 +0900

    [add] added support api

--- src/api/Support.ts
+++ src/api/Support.ts
@@ -24,3 +24,25 @@ export function Post(data: SupportAddData): Promise<{ error: string | undefined;
         };
     })
 }
+
+export function Put(id: number, data: any): Promise<{ error: string | undefined; data: any }> {
+    return axios.put(restfulApiConfig.apiURL + "/support/" + id, data, {
+        headers: {
+            'Content-Type': 'application/json',
+            USER_TOKEN: Cookies.get('user_token'),
+            ACCESS_TOKEN: Cookies.get('access_token'),
+        }
+    }).then(res => {
+        console.log(res);
+        return {
+            error: undefined,
+            data: res.data
+        };
+    }).catch(err => {
+        console.log(err.response);
+        return {
+            error: "[" + err.response.status + "] " + err.response.data.error,
+            data: null
+        };
+    })
+}
