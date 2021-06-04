a4aa2d83ef99e22c8726d3c72b7b2418d1e9d86f
yoneyan <github@yoneyan.net>
2021-06-05 04:24:57 +0900

    [add] Added a structure for request api

--- src/interface.ts
+++ src/interface.ts
@@ -2,42 +2,51 @@ export interface InfoData {
     user?: UserData
     group?: GroupData
     user_list?: UserData[]
-    // service?: ServiceData[]
+    service?: ServiceData[]
     info?: InfosData[]
     notice?: NoticeData[]
     ticket?: TicketData[]
 }
 
 export interface UserData {
-    id: number,
-    group_id: number,
-    name: string,
-    name_en: string,
-    email: string,
-    status: number,
-    level: number,
-    mail_verify: boolean,
+    id: number
+    group_id: number
+    name: string
+    name_en: string
+    email: string
+    status: number
+    level: number
+    mail_verify: boolean
 }
 
 export interface GroupData {
-    id: number,
-    agree: boolean,
-    question: string,
-    org: string,
-    org_en: string,
-    postcode: string,
-    address: string,
-    address_en: string,
-    tel: string,
-    country: string,
-    contract: string,
-    student: boolean,
-    student_expired: string,
-    fee: number,
-    lock: boolean,
-    expired_status: number,
-    status: number,
-    pass: boolean,
+    id: number
+    agree: boolean
+    question: string
+    org: string
+    org_en: string
+    postcode: string
+    address: string
+    address_en: string
+    tel: string
+    country: string
+    contract: string
+    student: boolean
+    student_expired: string
+    fee: number
+    lock: boolean
+    expired_status: number
+    status: number
+    pass: boolean
+    add_allow: boolean
+}
+
+export interface ServiceData {
+    id: number
+    service_id: string
+    service_type: string
+    need_route: boolean
+    add_allow: boolean
 }
 
 export interface NoticeData {
@@ -47,76 +56,76 @@ export interface NoticeData {
     fault: boolean
     important: boolean
     info: boolean
-    title: string,
-    data: string,
+    title: string
+    data: string
 }
 
 export interface TicketData {
-    id: number,
-    created_at: string,
-    group_id: number,
-    user_id: number,
-    title: string,
-    chat?: ChatData[],
-    solved: boolean,
+    id: number
+    created_at: string
+    group_id: number
+    user_id: number
+    title: string
+    chat?: ChatData[]
+    solved: boolean
 }
 
 export interface ChatData {
-    created_at: string,
-    ticket_id: number,
-    user_id: number,
-    user_name: string,
-    admin: boolean,
-    data: string,
+    created_at: string
+    ticket_id: number
+    user_id: number
+    user_name: string
+    admin: boolean
+    data: string
 }
 
 export interface InfosData {
-    service_id: number,
-    service: string,
-    assign: boolean,
-    asn: number,
-    v4?: string[],
-    v6?: string[],
-    noc: string,
-    noc_ip: string,
-    term_ip: string,
-    link_v4_our: string,
-    link_v4_your: string,
-    link_v6_our: string,
-    link_v6_your: string,
-    fee: string,
-    org: string,
-    org_en: string,
-    postcode: string,
-    address: string,
-    address_en: string,
-    jpnic_admin?: JPNICData,
-    jpnic_tech?: JPNICData[],
-    avg_downstream: number,
-    avg_upstream: number,
-    max_downstream: number,
-    max_upstream: number,
-    max_bandwidth_as: number,
-    bgp_route_v4: string,
-    bgp_route_v6: string,
+    service_id: number
+    service: string
+    assign: boolean
+    asn: number
+    v4?: string[]
+    v6?: string[]
+    noc: string
+    noc_ip: string
+    term_ip: string
+    link_v4_our: string
+    link_v4_your: string
+    link_v6_our: string
+    link_v6_your: string
+    fee: string
+    org: string
+    org_en: string
+    postcode: string
+    address: string
+    address_en: string
+    jpnic_admin?: JPNICData
+    jpnic_tech?: JPNICData[]
+    avg_downstream: number
+    avg_upstream: number
+    max_downstream: number
+    max_upstream: number
+    max_bandwidth_as: number
+    bgp_route_v4: string
+    bgp_route_v6: string
 }
 
 export interface JPNICData {
-    ID: number,
-    address: string,
-    address_en: string,
-    country: string,
-    dept: string,
-    dept_en: string,
-    fax: string,
-    jpnic_handle: string,
-    mail: string,
-    name: string,
-    name_en: string,
-    org: string,
-    org_en: string,
-    postcode: string,
-    tel: string,
+    ID: number
+    address: string
+    address_en: string
+    country: string
+    dept: string
+    dept_en: string
+    fax: string
+    jpnic_handle: string
+    mail: string
+    name: string
+    name_en: string
+    org: string
+    org_en: string
+    postcode: string
+    tel: string
     lock: boolean
 }
 
@@ -188,7 +197,7 @@ export interface TunnelEndPointRouterIPTemplateData {
     DeletedAt: string
     ID: number
     UpdatedAt: string
-    ip: string,
+    ip: string
     enable: boolean
     tunnel_endpoint_router: TunnelEndPointRouterTemplateData
 }
@@ -198,7 +207,7 @@ export interface ConnectionTemplateData {
     DeletedAt: string
     ID: number
     UpdatedAt: string
-    name: string,
+    name: string
     type: string
     comment: string
     need_comment: boolean
@@ -209,26 +218,27 @@ export interface ConnectionTemplateData {
 }
 
 export interface ServiceTemplateData {
-    ID: number,
-    name: string,
-    comment: string,
-    hidden: boolean,
+    ID: number
+    name: string
+    comment: string
+    hidden: boolean
     type: string
-    need_comment: boolean,
-    need_global_as: boolean,
-    need_jpnic: boolean,
+    need_comment: boolean
+    need_global_as: boolean
+    need_jpnic: boolean
     need_route: boolean
 }
 
 export interface IPRouteData {
-    ID: number,
-    CreatedAt: string,
-    UpdatedAt: string,
+    ID: number
+    CreatedAt: string
+    UpdatedAt: string
     name: string
 }
 
 export interface TemplateData {
     services?: ServiceTemplateData[]
+    connections?: ConnectionTemplateData[]
     ipv4?: IPTemplateData[]
     ipv6?: IPTemplateData[]
     nocs?: NocTemplateData[]
@@ -239,72 +249,94 @@ export interface TemplateData {
     ipv6_route?: IPRouteData[]
 }
 
+export interface GroupAddData {
+    agree: boolean
+    question: string
+    org: string
+    org_en: string
+    postcode: string
+    address: string
+    address_en: string
+    tel: string
+    country: string
+    contract: string
+    student: boolean
+    student_expired: string
+}
+
 export interface ServiceAddData {
-    jpnic_admin?: ServiceAddJPNICData,
-    jpnic_tech?: ServiceAddJPNICData[],
-    service_template_id: number,
-    service_comment: string,
-    org?: string,
-    org_en?: string,
-    postcode?: string,
-    address?: string,
-    address_en?: string,
-    route_v4?: string,
-    route_v6?: string,
-    avg_upstream: number,
-    max_upstream: number,
-    avg_downstream: number,
-    max_downstream: number,
-    max_bandwidth_as?: string,
-    asn?: number,
-    ip?: ServiceAddIPData[],
-    start_date: string,
+    jpnic_admin?: ServiceAddJPNICData
+    jpnic_tech?: ServiceAddJPNICData[]
+    service_template_id: number
+    service_comment: string
+    org?: string
+    org_en?: string
+    postcode?: string
+    address?: string
+    address_en?: string
+    route_v4?: string
+    route_v6?: string
+    avg_upstream: number
+    max_upstream: number
+    avg_downstream: number
+    max_downstream: number
+    max_bandwidth_as?: string
+    asn?: number
+    ip?: ServiceAddIPData[]
+    start_date: string
     end_date?: string
 }
 
 export interface ServiceAddJPNICData {
-    org: string,
-    org_en: string,
-    mail: string,
-    postcode: string,
-    address: string,
-    address_en: string,
-    name: string,
-    name_en: string,
-    dept_en: string,
-    dept: string,
-    country: string,
-    tel: string,
-    fax: string,
+    org: string
+    org_en: string
+    mail: string
+    postcode: string
+    address: string
+    address_en: string
+    name: string
+    name_en: string
+    dept_en: string
+    dept: string
+    country: string
+    tel: string
+    fax: string
 }
 
 export interface ServiceAddIPData {
-    version: number,
-    ip: string,
-    plan?: ServiceAddIPv4PlanData[],
-    name: string,
-    start_date: string,
+    version: number
+    ip: string
+    plan?: ServiceAddIPv4PlanData[]
+    name: string
+    start_date: string
     end_date?: string
 }
 
 export interface ServiceAddIPv4PlanData {
-    name: string,
-    after: number,
-    half_year: number,
-    one_year: number,
+    name: string
+    after: number
+    half_year: number
+    one_year: number
 }
 
 export interface ConnectionAddData {
-    address: string,
-    connection_template_id: number,
-    connection_comment: string,
-    ipv4_route_template_id?: number,
-    ipv6_route_template_id?: number,
-    ntt_template_id: number,
-    noc_id: number,
-    term_ip: string,
+    address: string
+    connection_template_id: number
+    connection_comment: string
+    ipv4_route_template_id?: number
+    ipv6_route_template_id?: number
+    ntt_template_id: number
+    noc_id: number
+    term_ip: string
     monitor: boolean
 }
+
+export interface SupportAddData {
+    is_group: boolean
+    title: string
+    data: string
+}
+
 
 export const DefaultTemplateData: TemplateData = {
     services: undefined,
@@ -314,6 +346,21 @@ export const DefaultTemplateData: TemplateData = {
     ntts: undefined,
     tunnel_endpoint_router: undefined,
     tunnel_endpoint_router_ip: undefined,
+}
+
+export const DefaultGroupAddData: GroupAddData = {
+    agree: false,
+    question: "",
+    org: "",
+    org_en: "",
+    postcode: "",
+    address: "",
+    address_en: "",
+    tel: "",
+    country: "",
+    contract: "E-Mail",
+    student: false,
+    student_expired: "",
 }
 
 export const DefaultServiceAddData: ServiceAddData = {
@@ -379,4 +426,10 @@ export const DefaultAddIP: ServiceAddIPData = {
     name: "",
     start_date: "",
     end_date: undefined
+}
+
+export const DefaultSupportAddData: SupportAddData = {
+    is_group: true,
+    title: "",
+    data: ""
 }
