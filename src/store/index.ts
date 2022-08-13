44b9b11fcd3ccbb1ae5a4c214c852c921c783b4a
yoneyan <contact@yoneyan.net>
2022-08-13 18:35:16 +0900

    [update] Updated library.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,9 @@
 import {applyMiddleware, createStore} from "redux";
 import rootReducer from "./rootReducer";
 import logger from "redux-logger";
-import {composeWithDevTools} from "redux-devtools-extension";
 import {RootInfoState} from "./infoReducer";
 import {RootTemplateState} from "./templateReducer";
+import {composeWithDevTools} from "@redux-devtools/extension";
 
 
 const store = createStore(
